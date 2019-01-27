c41d07afd4cf3b76540e8263bbfbf87611552490
Drew T. Ritcher <ritchedt@miamioh.edu>
2019-01-26 19:37:22 -0500

    added algorithm to finding prime numbers based on square root methodology and added tests to accompany several different use cases. Also added logic based on validating proper range values

--- lib/prime.rb
+++ lib/prime.rb
@@ -10,8 +10,22 @@ class Prime
 
 	def store_primes_from_range(input_1, input_2)
 		return @stored_primes unless input_1.is_a?(Integer) && input_2.is_a?(Integer)
+		return @stored_primes unless input_1 >= 0 && input_2 >= 0
 
-		@stored_primes = [2,3,5,7]
+		max = input_2 > input_1 ? input_2 : input_1
+		min = input_2 < input_1 ? input_2 : input_1
+		@stored_primes = (2..max).to_a
+
+		(2..Math.sqrt(max).floor).each_with_index do |value, index|
+			current_prime = @stored_primes.at(index)
+			@stored_primes.each do |num|
+				if num % current_prime == 0 && current_prime != num
+					@stored_primes.delete(num)
+				end
+			end
+		end
+		
+		@stored_primes.delete_if {|value| value < min}
 	end
 
 end
--- spec/prime_spec.rb
+++ spec/prime_spec.rb
@@ -22,7 +22,7 @@ describe 'Testing the units in prime class' do
     	expect(@test.stored_primes).not_to be_empty
 	end
 
-	it 'only accepts valid range arguments' do
+	it 'only accepts valid class type arguments' do
     	@test.store_primes_from_range("1", 10)
     	@test.store_primes_from_range(1, "10")
     	@test.store_primes_from_range('2', '3')
@@ -30,9 +30,50 @@ describe 'Testing the units in prime class' do
     	expect(@test.stored_primes).to be_empty
 	end
 
-	it 'gets all prime numbers in a small range' do
+	it 'accepts inverse ranges' do
+		@test.store_primes_from_range(10, 0)
+    	expect(@test.stored_primes).to eq [2,3,5,7]
+	end
+
+	it 'gets all prime numbers of small range of lower numbers' do
 		@test.store_primes_from_range(0, 10)
     	expect(@test.stored_primes).to eq [2,3,5,7]
 	end
 
+	it 'gets all prime numbers of small range of large numbers' do
+		@test.store_primes_from_range(7900, 7920)
+    	expect(@test.stored_primes).to eq [7901, 7907, 7919]
+	end
+
+	it 'gets all prime numbers from 0 to 101' do
+		@test.store_primes_from_range(0, 101)
+		expect(@test.stored_primes).to eq [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 
+			41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101]
+	end
+
+	it 'will not accept negative values as arguments' do
+		@test.store_primes_from_range(-1, 10)
+		@test.store_primes_from_range(10, -1)
+    	expect(@test.stored_primes).to be_empty
+	end
+
+	it 'will not return any prime numbers if input values are less than 2' do
+		@test.store_primes_from_range(0, 1)
+    	expect(@test.stored_primes).to be_empty
+	end
+
+	it 'will return any prime number if max value is prime' do
+		@test.store_primes_from_range(0, 2)
+    	expect(@test.stored_primes).to eq [2]
+	end
+
+	it 'gets all prime numbers of inclusive range with prime' do
+		@test.store_primes_from_range(3, 3)
+    	expect(@test.stored_primes).to eq [3]
+	end
+
+	it 'gets all prime numbers of inclusive range without prime' do
+		@test.store_primes_from_range(10, 10)
+    	expect(@test.stored_primes).to be_empty
+	end
 end
